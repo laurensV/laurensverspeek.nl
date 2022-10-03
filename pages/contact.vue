d5073cfe981a9d6f0bbeb16abb0c4843b2a09e5a
Laurens Verspeek <l.verspeek@gmail.com>
2022-10-03 16:22:16 +0200

    add contact page

---
+++ pages/contact.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="container is-max-widescreen">
+      <div class="mt-6">
+        <h1> Contact </h1>
+        <div class="columns">
+          <div class="column">
+            <h4><a href="mailto:contact@laurensverspeek.nl"> <i class="fas fa-envelope mr-1" aria-hidden="true"></i> email me</a></h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+<style lang="scss">
+
+</style>
