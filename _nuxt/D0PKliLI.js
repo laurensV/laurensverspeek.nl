c51e82a3b60369e8ac2a65d6cbd11655a65d5d45
laurensV <laurensV@users.noreply.github.com>
2026-07-14 14:26:07 +0000

    deploy: 4134207e657eee06bc6207081079bb042795cf03

---
+++ _nuxt/D0PKliLI.js
@@ -0,0 +1,1 @@
+import{_ as a,o as s,c as o,a0 as r}from"./DtVGLS-D.js";const t={};function n(e,c){return s(),o("table",null,[r(e.$slots,"default")])}const _=Object.assign(a(t,[["render",n]]),{__name:"ProseTable"});export{_ as default};
